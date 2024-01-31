d343b8c8288c82350a868a33c1668db76d0a11d4
Y A S <54225481+specialyas@users.noreply.github.com>
2024-01-31 11:59:44 +0100

    set different timer title

--- main.py
+++ main.py
@@ -26,10 +26,13 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
+        label_title.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
+        label_title.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
+        label_title.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
