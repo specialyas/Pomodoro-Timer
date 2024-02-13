4a049f23e238c7a186be2aa9356a674264355822
Y A S <54225481+specialyas@users.noreply.github.com>
2024-02-13 12:45:44 +0100

    modify styling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
+y_GREEN = "#12372A"
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -15,7 +16,6 @@ timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
 def reset_timer():
     window.after_cancel(timer)
     # timer_text 00:00
@@ -69,24 +69,24 @@ def count_down(count):
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=50, bg=y_GREEN)
 
-label_title = Label(text="Pomodoro Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+label_title = Label(text="Pomodoro Timer", fg=GREEN, bg=y_GREEN, font=(FONT_NAME, 50))
 label_title.grid(column=1, row=0)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=y_GREEN, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 138, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 138, text='00:00', fill=y_GREEN, font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", highlightthickness=0, font=(FONT_NAME, 30), command=start_timer)
+start_button = Button(text="Start", highlightthickness=0, bg=GREEN, fg=y_GREEN,  font=(FONT_NAME, 30), command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0, font=(FONT_NAME, 30), command=reset_timer)
+reset_button = Button(text="Reset", highlightthickness=0, bg=GREEN, fg=y_GREEN, font=(FONT_NAME, 30), command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text='', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
+check_marks = Label(text='', fg=GREEN, bg=y_GREEN, font=(FONT_NAME, 30))
 check_marks.grid(column=1, row=2)
 
 window.mainloop()
